f4ff2f3bb68e90be012cb9ac1dc9dd2ce9e11b82
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2023-06-07 15:03:48 +0900

    Time: 83 ms (36.69%), Space: 44.9 MB (71.21%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let frequency = {};
+    for(let i = 0; i < nums.length; i++){
+        if(frequency.hasOwnProperty(nums[i])) frequency[nums[i]] += 1;
+        else frequency[nums[i]] = 1;
+    }
+    let result = Object.keys(frequency).map((key) => [Number(key), frequency[key]]);
+    let sortedResult = result.sort((a,b) => {
+        return b[1] - a[1];
+    });
+    let answer = [];
+    for (let i = 0; i < k; i++){
+        answer.push(sortedResult[i][0]);
+    }
+    return answer;
+};
