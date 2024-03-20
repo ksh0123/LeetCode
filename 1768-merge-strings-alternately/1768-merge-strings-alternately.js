1e8e8240a116ae9b3479401df5cfa319fd7e260a
Shinhye Kang <122417190+ksh0123@users.noreply.github.com>
2024-03-20 11:49:29 +0900

    Time: 51 ms (72.76%), Space: 49.6 MB (50.71%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,15 +4,13 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let answer = '';
-    let index = 0;
-    while(true){
-        if(word1[index] === undefined && word2[index] === undefined){
-            return answer;
-        } else {
-            if(word1[index] !== undefined) answer += word1[index];
-            if(word2[index] !== undefined) answer += word2[index];
-            index++;
-        }
+    let answer = [];
+    const maxLength = Math.max(word1.length, word2.length);
+
+    for (let index = 0; index < maxLength; index++) {
+        if (index < word1.length) answer.push(word1[index]);
+        if (index < word2.length) answer.push(word2[index]);
     }
+
+    return answer.join('');
 };
