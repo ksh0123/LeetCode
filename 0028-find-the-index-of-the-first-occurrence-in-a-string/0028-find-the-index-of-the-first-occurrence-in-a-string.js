41efa3c8df274698466bf05803ebcdd7a42dad6f
shnae <shnae.k@gmail.com>
2024-10-03 23:48:28 +0900

    Time: 48 ms (79.81%), Space: 48.7 MB (69.57%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(!haystack.includes(needle)) return -1;
+    return haystack.split(`${needle}`)[0].length;
+};
